276fbc25b6fcbf26b94ec912b022cab29a8aeb1b
stonedch <stch.max.muhin@gmail.com>
2020-08-10 17:49:04 +0300

    feat: add back function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 function insert(num) {
     document.form.textview.value = document.form.textview.value + num;
 }
+
+function back() {
+    var exp = document.form.textview.value;
+    document.form.textview.value = exp.substring(0, exp.length-1)
+}
