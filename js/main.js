825db3a67f5a263dbc73e2ed6cbdf93a922841fa
stonedch <stch.max.muhin@gmail.com>
2020-08-10 17:52:04 +0300

    feat: add clean function

--- js/main.js
+++ js/main.js
@@ -6,3 +6,7 @@ function back() {
     var exp = document.form.textview.value;
     document.form.textview.value = exp.substring(0, exp.length-1)
 }
+
+function clean() {
+    document.form.textview.value = "";
+}
